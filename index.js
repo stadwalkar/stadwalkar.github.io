a3627828611016d088e3336651d1a05615fae735
stadwalkar <sahiltadwalkar@gmail.com>
2025-01-31 12:50:31 -0800

    app

---
+++ index.js
@@ -0,0 +1,5 @@
+// This file is just to help Vite process the HTML files
+import './index.html'
+import './real-estate.html'
+import './startups.html'
+import './connect.html' 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        realEstate: '/real-estate.html',
+        startups: '/startups.html',
+        connect: '/connect.html',
+      },
+    },
+  },
   server: {
     open: true
   }
